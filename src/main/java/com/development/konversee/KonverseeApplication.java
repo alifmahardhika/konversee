0c63c482d410c0545f03d70764af22f0684b41f9
Alif Mahardhika <alif.mahardhika@ui.ac.id>
2019-12-21 00:19:54 +0700

    initial commit

---
+++ src/main/java/com/development/konversee/KonverseeApplication.java
@@ -0,0 +1,13 @@
+package com.development.konversee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonverseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonverseeApplication.class, args);
+	}
+
+}
